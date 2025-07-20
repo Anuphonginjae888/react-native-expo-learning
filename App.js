7b1b25482d099e5ba6c2b38a9964797f5fb8327e
Anuphong injae <tongamepro83@gmail.com>
2025-07-20 16:33:00 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>นายอณุพงษ์ อินแจ้ ทธด.102</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#00FFFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
