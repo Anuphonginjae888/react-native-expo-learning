72631c0ce715738d43c957cc9be8ff67c92d84b1
Anuphong injae <tongamepro83@gmail.com>
2025-07-27 17:18:45 +0700

    feat: set up React Navigation with Home Details and Profile screens

--- App.js
+++ App.js
@@ -1,20 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import Navigation from "./components/Navigation";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>นายอณุพงษ์ อินแจ้ ทธด.102</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Navigation />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#00FFFF',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
